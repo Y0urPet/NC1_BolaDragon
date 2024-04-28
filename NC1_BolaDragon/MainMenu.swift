e7bb3c3928837ef2e951c6b6f6eab702d37eb411
Christian Gunawan <cg404cgunawan@gmail.com>
2024-04-28 15:27:14 +0700

    nav link woi

--- NC1_BolaDragon/MainMenu.swift
+++ NC1_BolaDragon/MainMenu.swift
@@ -6,44 +6,44 @@
 //
 
 import SwiftUI
+import RealityKit
+
 
 struct MainMenu: View {
     var body: some View {
-        ZStack {
-            Color.forestGreen.ignoresSafeArea()
-            CubeBackground()
-            Color.black.opacity(0.7).ignoresSafeArea()
-            VStack(spacing: 50) {
-                DragonBallTitle()
-                HStack(spacing: 16) {
-                    Button {
-                        let playNavigation = NavigationLink(destination: AR())
+        NavigationView {
+            ZStack {
+                Color.forestGreen.ignoresSafeArea()
+                CubeBackground()
+                Color.black.opacity(0.7).ignoresSafeArea()
+                VStack(spacing: 50) {
+                    DragonBallTitle()
+                    HStack(spacing: 16) {
+                        NavigationLink(destination: ARViewContainer()) {
+                            Image(systemName: "play.fill")
+                                .resizable()
+                                .foregroundColor(.prime)
+                                .frame(width: 30, height: 30)
+                                .padding(25)
+                                .background(.second)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        }
                         
-                    } label: {
-                        Image(systemName: "play.fill")
-                            .resizable()
-                            .foregroundStyle(.prime)
-                            .frame(width: 30,height: 30)
+                        Button(action: {
+                            // Add action for the second button here
+                        }) {
+                            Image("achievement")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .padding(20)
+                                .background(.second)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        }
                     }
-                    .padding(25)
-                    .background(.second)
-                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                    Button {
-                        
-                    } label: {
-                        Image("achievement")
-                            .resizable()
-                            .frame(width: 40,height: 40)
-                    }
-                    .padding(20)
-                    .background(.second)
-                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
                 }
             }
         }
     }
 }
 
-#Preview {
-    MainMenu()
-}
+
