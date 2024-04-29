8e1b2fbbacb603c49467b913046089534d0ca371
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2024-04-29 10:26:54 +0700

    add achievement page

---
+++ NC1_BolaDragon/Achievement.swift
@@ -0,0 +1,18 @@
+//
+//  Achievement.swift
+//  NC1_BolaDragon
+//
+//  Created by Stefan Agusto Hutapea on 29/04/24.
+//
+
+import SwiftUI
+
+struct Achievement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Achievement()
+}
